11bb96581eaaf1fd17e1ed98f62feff8d92448e1
Evan G Timmons <evangeorgetimmons@gmail.com>
2018-10-10 00:19:55 -0500

    Added new solution for leetcode problem

--- Java/src/removeDuplicatesFromSortedArray.java
+++ Java/src/removeDuplicatesFromSortedArray.java
@@ -1,7 +1,26 @@
 class Solution {
     public int removeDuplicates(int[] nums) {
         // Counts the amount of unique numbers
-        int uniqueNum = 0;
+        // Assumes one unique number
+        int uniqueNum = 1;
 
+        // Length of original number array
+        int numLength = nums.length;
+
+        if(numLength == 0){
+            return 0;
+        }
+
+        // Iterates through array once
+        for(int i = 1; i < numLength; i++){
+            // Checks for a unique number
+            if(nums[i] != nums[i - 1]){
+                // Adds the number and increments the count
+                nums[uniqueNum] = nums[i];
+                uniqueNum ++;
+            }
+        }
+
+        return uniqueNum;
     }
 }
