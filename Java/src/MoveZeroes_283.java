edec1597844d3f55be83406b9e4a85f191acbe3f
Evan Timmons <evangeorgetimmons@gmail.com>
2020-06-19 17:01:48 -0500

    283 with a lower theoretical runtime, but longer actual runtime

--- Java/src/MoveZeroes_283.java
+++ Java/src/MoveZeroes_283.java
@@ -1,30 +1,30 @@
 import java.lang.reflect.Array;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.LinkedHashSet;
+import java.util.*;
 
 public class MoveZeroes_283 {
 
     public static void main(String[] args) {
-        int[] sample = new int[]{0, 0, 1, 2, 0, 3, 0, 0, 0};
+        int[] sample = new int[]{1,1};
         moveZeroes(sample);
         System.out.println(Arrays.toString(sample));
     }
 
     public static void moveZeroes(int[] nums) {
-        ArrayList<Integer> list = new ArrayList<>();
+        Queue<Integer> numQueue = new LinkedList<>();
+        Queue<Integer> replaceIndexQ = new LinkedList<>();
+        int replaceIndex = 0;
 
-        for (int num : nums) {
-            if (num != 0) {
-                list.add(num);
-            }
-        }
 
         for (int i = 0; i < nums.length; i++) {
-            if(i < list.size()) {
-                nums[i] = list.get(i);
-            } else {
+            if (nums[i] != 0) {
+                numQueue.add(nums[i]);
+                replaceIndexQ.add(replaceIndex);
+                replaceIndex++;
+            }
+
+            if (!numQueue.isEmpty()) {
                 nums[i] = 0;
+                nums[replaceIndexQ.poll()] = numQueue.poll();
             }
         }
     }
