dfe24c8a865d50c54dcb3746163bdba714aca27d
Evan Timmons <evangeorgetimmons@gmail.com>
2020-06-18 21:49:27 -0500

    Working rotate array

--- Java/src/rotateArray_189.java
+++ Java/src/rotateArray_189.java
@@ -1,22 +1,15 @@
 class rotateArray_189 {
-
-    public static void main(String[] args) {
-        int[] nums = {1,2,3,4,5,6,7};
-        int k = 3;
-        int[] result;
-        result = rotate(nums, k);
-        System.out.println(result[0]);
-    }
-
-    public static int[] rotate(int[] nums, int k) {
+    public static void rotate(int[] nums, int k) {
         int[] result = new int[nums.length];
         int shiftedPosition;
 
-        for(int i = 0; i < k; i++) {
+        for(int i = 0; i < nums.length; i++) {
             shiftedPosition = (k + i) % nums.length;
-            result[i] = nums[shiftedPosition];
+            result[shiftedPosition] = nums[i];
         }
 
-        return result;
+        for (int j = 0; j < nums.length; j++) {
+            nums[j] = result[j];
+        }
     }
 }
