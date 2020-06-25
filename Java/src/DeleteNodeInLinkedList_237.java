587b876a2bc7e05c03975bef20978d343a3b170f
Evan Timmons <evangeorgetimmons@gmail.com>
2020-06-25 17:14:12 -0500

    237 completed with optimal solution

---
+++ Java/src/DeleteNodeInLinkedList_237.java
@@ -0,0 +1,46 @@
+import java.util.List;
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ * int val;
+ * ListNode next;
+ * ListNode(int x) { val = x; }
+ * }
+ */
+public class DeleteNodeInLinkedList_237 {
+    public static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+        }
+    }
+
+    public static void main(String[] args) {
+        ListNode initial = new ListNode(4);
+        ListNode nodeToDelete = new ListNode(5);
+        initial.next = nodeToDelete;
+        initial.next.next = new ListNode(1);
+        initial.next.next.next = new ListNode(9);
+        deleteNode(nodeToDelete);
+        printList(initial);
+    }
+
+    // !!!This is the function for submission!!!
+    public static void deleteNode(ListNode node) {
+        node.val = node.next.val;
+        node.next = node.next.next;
+    }
+
+    public static void printList(ListNode listHead) {
+        ListNode node = listHead;
+        System.out.print(node.val);
+        while (node.next != null) {
+            node = node.next;
+            System.out.print(" -> ");
+            System.out.print(node.val);
+        }
+    }
+}
