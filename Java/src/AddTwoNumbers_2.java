5561330a100b99be22490be46cdf177304e70730
Evan Timmons <evangeorgetimmons@gmail.com>
2020-06-25 18:16:45 -0500

    Solution accepted after a few attempts

--- Java/src/AddTwoNumbers_2.java
+++ Java/src/AddTwoNumbers_2.java
@@ -29,15 +29,15 @@ class AddTwoNumbers_2 {
     }
 
     public static void main (String[] args) {
-        ListNode l1 = new ListNode(1);
-        l1.next = new ListNode(2);
-        l1.next.next = new ListNode(3);
-        l1.next.next.next = new ListNode(4);
+        ListNode l1 = new ListNode(5);
+//        l1.next = new ListNode(2);
+//        l1.next.next = new ListNode(3);
+//        l1.next.next.next = new ListNode(4);
 
-        ListNode l2 = new ListNode(1);
-        l2.next = new ListNode(2);
-        l2.next.next = new ListNode(3);
-        l2.next.next.next = new ListNode(4);
+        ListNode l2 = new ListNode(5);
+//        l2.next = new ListNode(2);
+//        l2.next.next = new ListNode(3);
+//        l2.next.next.next = new ListNode(4);
 
         ListNode l3 = addTwoNumbers(l1, l2);
 
@@ -48,41 +48,55 @@ class AddTwoNumbers_2 {
 
     // Main function (SUBMIT)
     public static ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        int numberOne = l1.val;
-        int oneTensCounter = 10;
-        int numberTwo = l2.val;
-        int twoTensCounter = 10;
-        int sum = 0;
+        int l1Val = l1.val;
+        int l2Val = l2.val;
+        int sum = l1Val + l2Val;
+        ListNode initial;
+        int carryOver = 0;
 
-        while (l1.next != null) {
-            l1 = l1.next;
-            numberOne += oneTensCounter * l1.val;
-            oneTensCounter *= 10;
+        if (sum < 10) {
+            initial = new ListNode(sum);
+        } else {
+            initial = new ListNode(sum % 10);
+            carryOver = 1;
+            if (l1.next == null && l2.next == null) {
+                initial.next = new ListNode(1);
+                return initial;
+            }
+        }
+        ListNode loopNode = initial;
+
+        while (l1.next != null || l2.next != null) {
+            if (l1.next != null) {
+                l1 = l1.next;
+                l1Val = l1.val;
+            } else {
+                l1Val = 0;
+            }
+
+            if (l2.next != null) {
+                l2 = l2.next;
+                l2Val = l2.val;
+            } else {
+                l2Val = 0;
+            }
+
+            sum = l1Val + l2Val + carryOver;
+            carryOver = 0;
+            if (sum < 10) {
+                loopNode.next = new ListNode(sum);
+            } else {
+                loopNode.next = new ListNode(sum % 10);
+                carryOver = 1;
+            }
+            loopNode = loopNode.next;
         }
 
-        while (l2.next != null) {
-            l2 = l2.next;
-            numberTwo += twoTensCounter * l2.val;
-            twoTensCounter *= 10;
+        if (carryOver == 1) {
+            loopNode.next = new ListNode(1);
         }
 
-        sum = numberOne + numberTwo;
-        return sumToList(sum);
-    }
-
-    // Helper method needed for Leetcode solution (SUBMIT)
-    public static ListNode sumToList(int sum) {
-        ListNode prevNode;
-        ListNode initialNode = new ListNode(sum % 10);
-        sum = sum / 10;
-        prevNode = initialNode;
-        while (sum > 0) {
-            ListNode newNode = new ListNode(sum % 10);
-            prevNode.next = newNode;
-            sum = sum / 10;
-            prevNode = newNode;
-        }
-        return initialNode;
+        return initial;
     }
 
     // Self created testing helper method
