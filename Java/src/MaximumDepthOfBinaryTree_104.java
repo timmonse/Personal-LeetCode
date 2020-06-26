f0a56d46c314ab1d82decba83ae7d2cd3ce44bb1
Evan Timmons <evangeorgetimmons@gmail.com>
2020-06-26 10:08:01 -0500

    Completed with an optimal solution after a few attempts

---
+++ Java/src/MaximumDepthOfBinaryTree_104.java
@@ -0,0 +1,50 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ * int val;
+ * TreeNode left;
+ * TreeNode right;
+ * TreeNode() {}
+ * TreeNode(int val) { this.val = val; }
+ * TreeNode(int val, TreeNode left, TreeNode right) {
+ * this.val = val;
+ * this.left = left;
+ * this.right = right;
+ * }
+ * }
+ */
+class MaximumDepthOfBinaryTree_104 {
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public int maxDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        if (root.left == null && root.right == null) {
+            return 1;
+        } else if (root.left == null) {
+            return 1 + maxDepth(root.right);
+        } else if (root.right == null) {
+            return 1 + maxDepth(root.left);
+        } else {
+            return Math.max(1 + maxDepth(root.left), 1 + maxDepth(root.right));
+        }
+    }
+}
