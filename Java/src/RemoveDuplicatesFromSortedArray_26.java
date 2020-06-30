737298e618e5a93a8b7705c2f1196a81480b3dad
Evan Timmons <evangeorgetimmons@gmail.com>
2020-06-30 11:13:52 -0500

    Revisited the problem with a very similar but cleaner solution

--- Java/src/RemoveDuplicatesFromSortedArray_26.java
+++ Java/src/RemoveDuplicatesFromSortedArray_26.java
@@ -1,26 +1,16 @@
-class removeDuplicatesFromSortedArray_26 {
+class RemoveDuplicatesFromSortedArray_26 {
     public int removeDuplicates(int[] nums) {
-        // Counts the amount of unique numbers
-        // Assumes one unique number
-        int uniqueNum = 1;
-
-        // Length of original number array
-        int numLength = nums.length;
-
-        if(numLength == 0){
+        int length = nums.length;
+        if (length == 0) {
             return 0;
         }
-
-        // Iterates through array once
-        for(int i = 1; i < numLength; i++){
-            // Checks for a unique number
-            if(nums[i] != nums[i - 1]){
-                // Adds the number and increments the count
-                nums[uniqueNum] = nums[i];
-                uniqueNum ++;
+        int replacementIndex = 0;
+        for(int i = 1; i < length; i++) {
+            if (nums[i] != nums[replacementIndex]) {
+                replacementIndex++;
+                nums[replacementIndex] = nums[i];
             }
         }
-
-        return uniqueNum;
+        return replacementIndex + 1;
     }
 }
