8336ef99408270a9d4d992fc95245a2f8437cf5b
Evan Timmons <evangeorgetimmons@gmail.com>
2020-07-24 13:55:07 -0500

    Further simplified the solution

--- Java/src/MoveZeroes_283.java
+++ Java/src/MoveZeroes_283.java
@@ -3,26 +3,14 @@ import java.util.*;
 
 public class MoveZeroes_283 {
 
-    public static void main(String[] args) {
-        int[] sample = new int[]{1,1};
-        moveZeroes(sample);
-        System.out.println(Arrays.toString(sample));
-    }
-
     public static void moveZeroes(int[] nums) {
         int nonZeroCount = 0;
-        int zeroCount = 0;
-        int len = nums.length;
-        for (int i = 0; i < len; i++) {
+        for (int i = 0; i < nums.length; ++i) {
             if (nums[i] != 0) {
                 nums[nonZeroCount] = nums[i];
+                if (i != nonZeroCount) nums[i] = 0;
                 nonZeroCount++;
-            } else {
-                zeroCount++;
             }
-        }
-        for (int j = len - zeroCount; j < len; j++) {
-            nums[j] = 0;
         }
     }
 }
