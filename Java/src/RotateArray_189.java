b832b2d17ea08cae7278aad5a2ebfbd32812e037
Evan Timmons <evangeorgetimmons@gmail.com>
2020-07-24 12:28:31 -0500

    Rotates the array in place in O(n) time

--- Java/src/RotateArray_189.java
+++ Java/src/RotateArray_189.java
@@ -1,13 +1,27 @@
 class RotateArray_189 {
-    public static void rotate(int[] nums, int k) {
-        int[] result = new int[nums.length];
-        int shiftedPosition;
-
-        for(int i = 0; i < nums.length; i++) {
-            shiftedPosition = (k + i) % nums.length;
-            result[shiftedPosition] = nums[i];
+    public void rotate(int[] nums, int k) {
+        int len = nums.length;
+        if (k == 0 || k % len == 0) {
+            return;
         }
 
-        System.arraycopy(result, 0, nums, 0, nums.length);
+        int i = 0;
+        int old = nums[0];
+        int newNum;
+        int count = 0;
+        int cautionValue = 0;
+        while (count < len) {
+            int newPos = (i + k) % len;
+            newNum = nums[newPos];
+            nums[newPos] = old;
+            old = newNum;
+            i = newPos;
+            count++;
+            if (i == cautionValue) {
+                i += 1;
+                old = nums[i];
+                cautionValue = i;
+            }
+        }
     }
 }
