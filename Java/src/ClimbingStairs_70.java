a2a4a9dffbdf214969266e3b17eae55f491b2a6a
Evan Timmons <evangeorgetimmons@gmail.com>
2020-07-31 13:09:15 -0500

    Recursive solution using memoization

--- Java/src/ClimbingStairs_70.java
+++ Java/src/ClimbingStairs_70.java
@@ -1,18 +1,10 @@
 class ClimbingStairs_70 {
+    static int[] memo = new int[45];
     public int climbStairs(int n) {
-        if (n < 3) {
+        if (n==1 || n==2) {
             return n;
         }
-
-        int[] waysToTop = new int[n + 1];
-
-        waysToTop[1] = 1;
-        waysToTop[2] = 2;
-
-        for (int i = 3; i <= n; i++) {
-            waysToTop[i] = waysToTop[i - 1] + waysToTop[i - 2];
-        }
-
-        return waysToTop[n];
+        memo[n-1] = (memo[n-1] == 0) ? climbStairs(n-1) + climbStairs(n-2) : memo[n-1];
+        return memo[n-1];
     }
 }
