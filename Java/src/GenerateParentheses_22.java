267677a10f162db1f310c2c7cec44c07bd8fac7b
Evan Timmons <evangeorgetimmons@gmail.com>
2020-08-07 13:40:45 -0500

    Slightly reduced memory usage by changing the scope of the main list

--- Java/src/GenerateParentheses_22.java
+++ Java/src/GenerateParentheses_22.java
@@ -2,19 +2,19 @@ import java.util.ArrayList;
 import java.util.List;
 
 class GenerateParentheses_22 {
-    List<String> main = new ArrayList<>();
     public List<String> generateParenthesis(int n) {
-        rec("", n, 0);
+        List<String> main = new ArrayList<>();
+        rec(main, "", n, 0);
         return main;
     }
 
-    public void rec(String curr, int openCount, int closeCount) {
+    public void rec(List<String> main, String curr, int openCount, int closeCount) {
         if (openCount < 0 || closeCount < 0) return;
         if (openCount == 0 && closeCount == 0) {
             main.add(curr);
             return;
         }
-        rec(curr + "(", openCount - 1, closeCount + 1);
-        rec(curr + ")", openCount, closeCount - 1);
+        rec(main, curr + "(", openCount - 1, closeCount + 1);
+        rec(main, curr + ")", openCount, closeCount - 1);
     }
 }
