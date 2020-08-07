377eb509d988e7542174ea215cfa524ae5957ecd
Evan Timmons <evangeorgetimmons@gmail.com>
2020-08-07 13:37:45 -0500

    Simplified solution with faster run time

--- Java/src/GenerateParentheses_22.java
+++ Java/src/GenerateParentheses_22.java
@@ -2,22 +2,19 @@ import java.util.ArrayList;
 import java.util.List;
 
 class GenerateParentheses_22 {
+    List<String> main = new ArrayList<>();
     public List<String> generateParenthesis(int n) {
-        List<String> main = new ArrayList<String>();
-        listBuilder(main, "", 0, 0, n);
+        rec("", n, 0);
         return main;
     }
 
-    void listBuilder(List<String> main, String currString, int openNum, int closedNum, int n) {
-        if (currString.length() == n * 2) {
-            main.add(currString);
+    public void rec(String curr, int openCount, int closeCount) {
+        if (openCount < 0 || closeCount < 0) return;
+        if (openCount == 0 && closeCount == 0) {
+            main.add(curr);
             return;
         }
-        if (openNum < n) {
-            listBuilder(main, currString + "(", openNum + 1, closedNum, n);
-        }
-        if (closedNum < openNum) {
-            listBuilder(main, currString + ")", openNum, closedNum + 1, n);
-        }
+        rec(curr + "(", openCount - 1, closeCount + 1);
+        rec(curr + ")", openCount, closeCount - 1);
     }
 }
