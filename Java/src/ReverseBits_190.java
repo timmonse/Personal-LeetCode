4631c6521e42a4b9cf81c1adfa77256dbd2f41c7
Evan Timmons <evangeorgetimmons@gmail.com>
2020-08-08 12:17:58 -0500

    Simplified solution

--- Java/src/ReverseBits_190.java
+++ Java/src/ReverseBits_190.java
@@ -1,14 +1,14 @@
 public class ReverseBits_190 {
     // you need treat n as an unsigned value
     public int reverseBits(int n) {
-        int result = 0;
+        int reversed = 0;
 
-        for (int i = 0; i < 32; i++) {
-            result <<= 1;
-            result = ((n & 1) == 1) ? result + 1 : result;
-            n >>= 1;
+        for (int i = 32; i > 0; --i) {
+            reversed = reversed<<1;
+            reversed += n&1;
+            n = n>>1;
         }
 
-        return result;
+        return reversed;
     }
 }
