94fb348d52a11fbab0b7dd048442706e8bc3a3cb
Evan Timmons <evangeorgetimmons@gmail.com>
2020-08-09 22:26:05 -0500

    O(n) solution using a fast and slow node

---
+++ Java/src/MiddleOfTheLinkedList_876.java
@@ -0,0 +1,20 @@
+public class MiddleOfTheLinkedList_876 {
+    class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+            next = null;
+        }
+    }
+
+    public ListNode middleNode(ListNode head) {
+        ListNode fast = head, slow = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return slow;
+    }
+}
