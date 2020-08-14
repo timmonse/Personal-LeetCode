01d680f693c50633d7f7efeefe981995cf99ced4
Evan Timmons <evangeorgetimmons@gmail.com>
2020-08-14 13:55:49 -0500

    Completed the problem again using a custom comparator. Now the runtume is O(s * n log n) where s is the length of the longest log / string

--- Java/src/ReorderDataInLogFiles_937.java
+++ Java/src/ReorderDataInLogFiles_937.java
@@ -1,67 +1,43 @@
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
 public class ReorderDataInLogFiles_937 {
     public String[] reorderLogFiles(String[] logs) {
-        List<String> numberList = new ArrayList<>();
-        List<String> letterList = new ArrayList<>();
 
-        for (String log : logs) {
-            String movedOrEmpty = moveOrNumber(log);
-            if (movedOrEmpty.length() > 0) {
-                //letter
-                // Add altered log
-                letterList.add(movedOrEmpty);
-            } else {
-                //Number
-                // Add original log
-                numberList.add(log);
+        Comparator<String> comp = new Comparator<>() {
+
+            @Override
+            public int compare(String s1, String s2) {
+                boolean is1Alph;
+                boolean is2Alph;
+                int idx1 = s1.indexOf(' ');
+                int idx2 = s2.indexOf(' ');
+                is1Alph = Character.isLowerCase(s1.charAt(idx1+1));
+                is2Alph = Character.isLowerCase(s2.charAt(idx2+1));
+
+                if (!is1Alph) {
+                    if(!is2Alph) {
+                        return 0; // If both num, do nothing
+                    }
+                    return 1; // Num goes after alph
+                }
+
+                if (!is2Alph) {
+                    return -1;
+                }
+
+                int compared = s1.substring(idx1+1).compareTo(s2.substring(idx2+1));
+
+                // This case is a tie so revert to first code
+                if (compared == 0) {
+                    return s1.compareTo(s2);
+                } else {
+                    return compared;
+                }
             }
-        }
+        };
 
-        Collections.sort(letterList);
 
-        // Reorder
-        for (int i = 0; i < letterList.size(); ++i) {
-            letterList.set(i, moveToFront(letterList.get(i)));
-        }
-
-        String[] result = new String[logs.length];
-        int i;
-        for (i = 0; i < letterList.size(); ++i) {
-            result[i] = letterList.get(i);
-        }
-        for (int j = i; j - i < numberList.size(); ++j) {
-            result[j] = numberList.get(j-i);
-        }
-
-        return result;
-    }
-
-    // If a number, move code to back
-    // Otherwise return empty String
-    public String moveOrNumber(String log) {
-        char c = log.charAt(0);
-        int i;
-        for (i = 0; c != ' '; ++i) {
-            c = log.charAt(i);
-        }
-        c = log.charAt(i);
-        // If number, return empty String
-        if (!Character.isLowerCase(c)) {
-            return "";
-        }
-        return log.substring(i) + " " + log.substring(0,i-1);
-    }
-
-    // Return a String with the last value moved to the front
-    public String moveToFront(String log) {
-        char c = log.charAt(0);
-        int i;
-        for (i = log.length() - 1; c != ' '; --i) {
-            c = log.charAt(i);
-        }
-        return log.substring(i+2) + " " + log.substring(0,i+1);
+        Arrays.sort(logs, comp);
+        return logs;
     }
 }
