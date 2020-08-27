5007c338e839ca8ddae9e5659422a3b40da2bfe1
Evan Timmons <evangeorgetimmons@gmail.com>
2020-08-27 17:41:10 -0500

    O(n) solution using depth first search

---
+++ Java/src/MinimumDepthOfBinaryTree_111.java
@@ -0,0 +1,13 @@
+public class MinimumDepthOfBinaryTree_111 {
+    public int minDepth(TreeNode root) {
+        if (root == null) return 0;
+        return recur(root);
+    }
+
+    public int recur(TreeNode root) {
+        if (root.right == null && root.left == null) return 1;
+        if (root.right == null) return recur(root.left) + 1;
+        if (root.left == null) return recur(root.right) + 1;
+        return Math.min(recur(root.left) + 1, recur(root.right) + 1);
+    }
+}
