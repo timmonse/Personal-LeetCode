9edbd3fd6fcff5334f602cbd20850947bf22bd2b
Evan Timmons <evangeorgetimmons@gmail.com>
2020-08-27 18:05:31 -0500

    O(n) solution based on Kadanes algorithm

---
+++ Java/src/MaximumSubArray_53.java
@@ -0,0 +1,10 @@
+public class MaximumSubArray_53 {
+    public int maxSubArray(int[] nums) {
+        int max = nums[0];
+        for (int i = 1; i < nums.length; ++i) {
+            if (nums[i - 1] > 0) nums[i] += nums[i-1];
+            max = Math.max(max, nums[i]);
+        }
+        return max;
+    }
+}
