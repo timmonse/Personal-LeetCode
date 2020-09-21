7897c17966b180df129e214f2a3d9c816c55bb1a
Evan Timmons <evangeorgetimmons@gmail.com>
2020-09-21 17:23:57 -0500

    Bottom up dynamic programming solution to the question

---
+++ Java/src/DecodeWays_91.java
@@ -0,0 +1,18 @@
+public class DecodeWays_91 {
+    public int numDecodings(String s) {
+        if (s == null || s.length() == 0) return 0;
+        int[] dp = new int[s.length() + 1];
+        dp[0] = 1;
+        dp[1] = s.charAt(0) - '0' > 0 ? 1 : 0;
+
+        for (int i = 2; i < s.length() + 1; ++i) {
+            if (s.charAt(i-1) - '0' > 0) {
+                dp[i] += dp[i-1];
+            }
+            if (Integer.parseInt(s.substring(i-2,i)) <= 26 && Integer.parseInt(s.substring(i-2,i)) >= 10) {
+                dp[i] += dp[i-2];
+            }
+        }
+        return dp[dp.length-1];
+    }
+}
