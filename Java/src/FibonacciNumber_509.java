e82455f072a63326707ff319ce91f089179ab7d0
Evan Timmons <evangeorgetimmons@gmail.com>
2020-10-08 10:36:55 -0500

    Solved in O(N) time with dynamic programming

---
+++ Java/src/FibonacciNumber_509.java
@@ -0,0 +1,16 @@
+public class FibonacciNumber_509 {
+    public int fib(int N) {
+        if (N == 0) return 0;
+        if (N == 1) return 1;
+        int[] dp = new int[N+1];
+        dp[0] = 0;
+        dp[1] = 1;
+
+
+        for (int i = 2; i <= N; ++i) {
+            dp[i] = dp[i-1] + dp[i-2];
+        }
+
+        return dp[N];
+    }
+}
